68d1eb5069bba8fc6ab91234ab214885dfc1f49e
mdtaju0908 <mdtaju4175@gmail.com>
2026-03-03 19:29:19 +0530

    theme code updated

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,19 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+type NextThemesProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
